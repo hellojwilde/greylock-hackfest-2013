1fdc8dc2430a14a25a389984df1bee41322f5cbc
Jonathan Wilde <hello@jwilde.me>
2013-07-27 18:11:19 -0700

    adding changes

--- ui/js/main.js
+++ ui/js/main.js
@@ -4,7 +4,56 @@
 
 window.Dprm = Ember.Application.create();
 
+/* Store */
+
+Dprm.Store = DS.Store.extend({
+  revision: 13,
+
+  // TODO <jwilde>: add real adapter here
+  adapter: DS.FixtureAdapter.create()
+});
+
+/* Structure: Song */
+
+Dprm.SongModel = DS.Model.extend({
+  votes: DS.attr("number"),
+  haveVoted: DS.attr("boolean"),
+
+  location: DS.attr("string"),   // some sort of URI-type construct for
+                                 // where a song is currently.
+
+  albumName: DS.attr("string"),
+  albumCover: DS.attr("string"), // data: url with an image
+  name: DS.attr("string")
+});
+
+Dprm.SongModel.FIXTURES = {
+  { votes: 5, haveVoted: false, location: "", albumName: "Wootalbum", name: "Wootsong" },
+  { votes: 2, haveVoted: true, location: "", albumName: "Wootalbum", name: "Wootsong2" }
+};
+
+Dprm.SongView = Ember.View.extend({
+  templateName: "view-song"
+});
+
+Dprm.SongController = Ember.ObjectController.extend({
+  voteUp: function () {
+    var model = this.get("model");
+    if (model.get("haveVoted")) return;
+
+    model.set("votes", model.get("votes") + 1);
+  },
+
+  voteDown: function () {
+    var model = this.get("model");
+    if (model.get("haveVoted")) return;
+
+    model.set("votes", model.get("votes") - 1);
+  }
+});
+
 /* Routes */
+
 Dprm.Router.map(function () {
   // XXX an index route is created automagically
 
@@ -15,34 +64,32 @@ Dprm.Router.map(function () {
   // TODO <jwilde>: add more routes for favorites and the like
 });
 
-/* Store & Models */
+/* Routes */
 
-Dprm.Store = DS.Store.extend({
-  revision: 13,
-  adapter: DS.FixtureAdapter.create()
+Dprm.Router.map(function () {
+  // XXX an index route is created automagically
+
+  this.resource('room', { path: '/room/:room_id' }, function () {
+    this.route('add');
+  });
+
+  // TODO <jwilde>: add more routes for favorites and the like.
 });
 
-// represents an arbitrary song. displayed by song view.
-Dprm.SongModel = DS.Model.extend({
-  option: DS.belongsTo("Dprm.OptionModel"),
+/* Route: Index */
 
-  albumName: DS.attr("string"),
-  albumCover: DS.attr("string"), // data: url with an image
-  name: DS.attr("string"),
+Dprm.IndexController = Ember.ObjectController.extend({
 
-  location: DS.attr("string")    // some sort of URI-type construct for
-                                 // where a song is currently.
 });
 
-// represents a song that's up for voting. contains a song.
-Dprm.OptionModel = DS.Model.extend({
-  song: DS.belongsTo("Dprm.SongModel"),
-  votes: DS.attr("number")
-});
+/* Route: Room  */
 
-/* Views */
-Dprm.SongView = Ember.View.extend({
-  templateName: "view-song"
-});
+Dprm.RoomIndexController = Ember.ObjectController.extend({
 
-/* Controllers */
+})
+
+/* Route: Room - Add Song */
+
+Dprm.RoomAddController = Ember.ObjectController.extend({
+
+});
