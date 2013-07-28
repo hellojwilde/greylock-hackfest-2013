5f9551187bd21e3f665cf52249c3a4dc4ef2d0bd
Bruce Spang <bruce@brucespang.com>
2013-07-27 21:47:37 -0700

    nice-ify

--- transport/src/test.js
+++ transport/src/test.js
@@ -2,6 +2,6 @@ var id = "" + randBetween(0,100);
 console.log("starting as " + id)
 
 var peer = new Peer(id, {host: 'localhost', port: 9000});
-var client = new raft(peer, id, function(data) {
+var client = new Raft(peer, function(data) {
     console.error("apply:", data)
 })
